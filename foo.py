e861e12dd5f6ae97f2da5e3cf40973aaf91e1c5e
victormarcelino <victor.pepy@gmail.com>
2012-12-24 12:27:11 -0300

    adição de foo.py

---
+++ foo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Foo(object):
+
+	def __init__(self, bar=23):
+		self.bar = bar
+
+	@property
+	def bar(self):
+		return self._bar
+
+	@bar.setter
+	def bar(self, bar):
+		self._bar = bar
